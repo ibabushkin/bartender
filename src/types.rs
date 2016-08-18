4323145573ba7c97916fa246cd3e6b689f719cd6
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-08-18 23:06:56 +0200

    Improved config file handling and added basic infrastucture

--- src/types.rs
+++ src/types.rs
@@ -6,41 +6,68 @@ use std::path::{Path,PathBuf};
 
 #[derive(Debug)]
 pub struct Configuration {
-    format: Formatter,
-    timers: Vec<Timer>,
-    fifos: Vec<Fifo>,
+    buffer: Buffer,
+    timers: Vec<(usize, Timer)>,
+    fifos: Vec<(usize, Fifo)>,
 }
 
 impl Configuration {
     pub fn from_config_file(file: &Path) -> ConfigResult<Configuration> {
         let cfg = try!(parse_config_file(file));
-        let formatter = try!(Formatter::new(&cfg));
+
+        let mut format_string = Vec::new();
+        let mut entries = Vec::new();
+
+        let format =
+            if let Some(&Value::List(ref l)) = cfg.lookup("format") {
+                l
+            } else {
+                return Err(ConfigurationError::MissingFormat);
+            };
+
+        for entry in format {
+            match entry {
+                &Value::Svalue(ScalarValue::Str(ref s)) =>
+                    format_string.push(s.clone()),
+                &Value::Group(ref s) =>
+                    if let Some(&Setting {
+                            name: _,
+                            value: Value::Svalue(ScalarValue::Str(ref name)),
+                        }) = s.get("name") {
+                        entries.push((name.clone(), format_string.len()));
+                        format_string.push(String::new());
+                    } else {
+                        return Err(ConfigurationError::IllegalFormat);
+                    },
+                _ => return Err(ConfigurationError::IllegalFormat),
+            }
+        }
 
         let mut timers = Vec::new();
         let mut fifos = Vec::new();
 
-        for name in formatter.get_names() {
-            let t = try!(get_child(&cfg, name, "type"));
+        for (ref name, index) in entries {
+            let t = try!(get_child(&cfg, &name, "type"));
             if t == "timer" {
-                let path = try!(get_child(&cfg, name, "command_path"));
-                timers.push(Timer {
-                    seconds: get_seconds(&cfg, name),
+                let path = try!(get_child(&cfg, &name, "command_path"));
+                timers.push((index, Timer {
+                    seconds: get_seconds(&cfg, &name),
                     command: PathBuf::from(path),
-                });
+                }));
             } else if t == "fifo" {
-                let path = try!(get_child(&cfg, name, "fifo_path"));
-                fifos.push(Fifo {
+                let path = try!(get_child(&cfg, &name, "fifo_path"));
+                fifos.push((index, Fifo {
                     path: PathBuf::from(path),
-                });
+                }));
             } else {
                 return Err(
-                    ConfigurationError::IllegalType(String::from(name))
+                    ConfigurationError::IllegalType(name.clone())
                 );
             }
         }
 
         Ok(Configuration {
-            format: formatter,
+            buffer: Buffer { entries: format_string },
             timers: timers,
             fifos: fifos,
         })
@@ -94,47 +121,6 @@ pub struct Fifo {
 }
 
 #[derive(Debug)]
-pub struct Formatter {
-    format_string: Vec<String>,
-    entries: Vec<(String, usize)>,
-}
-
-impl Formatter {
-    fn new(cfg: &Config) -> ConfigResult<Formatter> {
-        let mut format_string = Vec::new();
-        let mut entries = Vec::new();
-
-        let format =
-            if let Some(&Value::List(ref l)) = cfg.lookup("format") {
-                l
-            } else {
-                return Err(ConfigurationError::MissingFormat);
-            };
-
-        for entry in format {
-            match entry {
-                &Value::Svalue(ScalarValue::Str(ref s)) =>
-                    format_string.push(s.clone()),
-                &Value::Group(ref s) =>
-                    if let Some(&Setting {
-                            name: _,
-                            value: Value::Svalue(ScalarValue::Str(ref name)),
-                        }) = s.get("name") {
-                        entries.push((name.clone(), format_string.len()));
-                    } else {
-                        return Err(ConfigurationError::IllegalFormat);
-                    },
-                _ => return Err(ConfigurationError::IllegalFormat),
-            }
-        }
-
-        Ok(Formatter {
-            format_string: format_string,
-            entries: entries,
-        })
-    }
-
-    fn get_names(&self) -> Vec<&str> {
-        self.entries.iter().map(|&(ref n, _)| n.as_str()).collect()
-    }
+pub struct Buffer {
+    entries: Vec<String>,
 }
