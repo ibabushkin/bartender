572986cf6b04fd093b261fb06cafa8bace749ea9
Inokentiy Babushkin <inokentiy.babushkin@googlemail.com>
2016-10-09 18:25:25 +0200

    First attempt for a poll()-based merged FIFO implementation

--- src/c_helper.rs
+++ src/c_helper.rs
@@ -1,5 +1,7 @@
 use libc;
+
 use std::fs::File;
+use std::io::{BufRead,BufReader};
 use std::os::unix::io::AsRawFd;
 
 /// Do... unspeakable things.
@@ -28,3 +30,46 @@ pub fn setup_pollfd(fd: &File) -> libc::pollfd {
     }
 }
 
+pub fn poll(fds: &mut [libc::pollfd]) -> bool {
+    let poll_res = unsafe {
+        libc::poll(fds.as_mut_ptr(), fds.len() as u64, -1)
+    };
+    poll_res > 0
+}
+
+pub fn get_lines(fds: &[libc::pollfd],
+                 readers: &mut [(Vec<u8>, BufReader<File>)])
+    -> Option<Vec<Option<String>>> {
+    let fd_len = fds.len();
+    if fd_len == readers.len() {
+        let mut res = Vec::with_capacity(fd_len);
+        for (fd, &mut (ref mut buf, ref mut reader)) in
+            fds.iter().zip(readers) {
+            if fd.fd != reader.get_ref().as_raw_fd() {
+                return None;
+            }
+            res.push(if fd.revents & libc::POLLIN != 0 {
+                if reader.read_until(0xA, buf).is_ok() {
+                    if let Some(&c) = buf.last() {
+                        if c == 0xA { let _ = buf.pop(); }
+                        if let Ok(s) = String::from_utf8(buf.clone()) {
+                            buf.clear();
+                            Some(s)
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            } else {
+                None
+            });
+        }
+        Some(res)
+    } else {
+        None
+    }
+}
