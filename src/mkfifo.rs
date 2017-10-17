9391b39e59a23a83872647ce7ebe058aa61ec96d
Inokentiy Babushkin <twk@twki.de>
2017-10-17 11:40:14 +0200

    Improved fifo opening and creation.

--- src/mkfifo.rs
+++ src/mkfifo.rs
@@ -2,13 +2,18 @@ use libc;
 
 use std::fs::{File, OpenOptions};
 use std::os::unix::fs::FileTypeExt;
+use std::os::unix::ffi::OsStrExt;
 use std::path::Path;
 
 pub fn open_fifo(path: &Path) -> Option<File> {
+    let mut options = OpenOptions::new();
+    options.read(true);
+    options.write(true);
+
     // we open the file in read-write mode to prevent our poll()
     // hack from sending us `POLLHUP`s when no process is at the
     // other end of the pipe, so it blocks either way.
-    match OpenOptions::new().read(true).write(true).open(path) {
+    match options.open(path) {
         Ok(f) => {
             match f.metadata().map(|m| m.file_type().is_fifo()) {
                 Ok(true) => Some(f),
@@ -16,10 +21,10 @@ pub fn open_fifo(path: &Path) -> Option<File> {
             }
         }
         _ => {
-            let path_ptr = path.as_os_str().to_os_string().to_str().unwrap().as_ptr();
+            let path_ptr = path.as_os_str().as_bytes().as_ptr();
             let perms = libc::S_IRUSR | libc::S_IWUSR;
             let ret = unsafe { libc::mkfifo(path_ptr as *const i8, perms) };
-            if ret != 0 { None } else { open_fifo(path) }
+            if ret != 0 { None } else { options.open(path).ok() }
         }
     }
 }
